bb309b06f4115b5b6d51f313b8aef2df7cb63e16
Marco <empleomarcotapia@gmail.com>
2024-05-03 06:15:01 -0700

    Initial Commit

---
+++ AplicacionApi/AplicacionApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  AplicacionApiApp.swift
+//  AplicacionApi
+//
+//  Created by Marco on 3/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AplicacionApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
